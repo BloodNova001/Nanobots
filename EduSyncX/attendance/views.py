a3647d2831e5df522eef38476e39d07b981c6727
priyansh patel <98945306+BloodNova001@users.noreply.github.com>
2024-09-28 17:40:28 +0530

    Delete EduSyncX/attendance directory

--- EduSyncX/attendance/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Attendance
-
-def attendance_view(request):
-    attendance_data = Attendance.objects.all()
-    return render(request, 'attendance/attendance.html', {'attendance_data': attendance_data})
