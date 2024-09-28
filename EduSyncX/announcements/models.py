e8209d480c3716a53773c900861fb611fdfc3a45
priyansh patel <98945306+BloodNova001@users.noreply.github.com>
2024-09-28 18:36:19 +0530

    Delete EduSyncX directory

--- EduSyncX/announcements/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Announcement(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    date = models.DateField()
-
-    def __str__(self):
-        return f'{self.title} - {self.date}'
--- EduSyncX/announcements/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Announcement
-
-def announcements_view(request):
-    announcements_data = Announcement.objects.all()
-    return render(request, 'announcements/announcements.html', {'announcements_data': announcements_data})
